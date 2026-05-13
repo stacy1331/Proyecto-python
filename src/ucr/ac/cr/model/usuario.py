85491b38ce2885d6a73df3f5a182e54d60b90b6d
solanoramirezpedrojose-ux <solanoramirezpedrojose@gmail.com>
2026-05-13 14:19:10 -0600

    Pedro: ModelUsuarios
    Cree el model de usuarios

--- src/ucr/ac/cr/model/usuario.py
+++ src/ucr/ac/cr/model/usuario.py
@@ -1,5 +1,10 @@
-#hola
-#adios
-#me cago en teylor
+class Usuario:
+    def __init__(self, cedula, nombre, correo, contrasena, rol):
+        self.cedula = cedula
+        self.nombre = nombre
+        self.correo = correo
+        self.contrasena = contrasena
+        self.rol = rol
 
-#stacy
+    def __str__(self):
+        return f"Cedula: {self.cedula} - Nombre: {self.nombre} - Correo: {self.correo} - Contraseña: {self.contrasena} - Rol: {self.rol}"
